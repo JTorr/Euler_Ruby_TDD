726f7abece47abf79ba2340ef3ebb477551fb3e4
Julie Torres <reeddetorres@gmail.com>
2015-05-16 22:52:38 -0400

    Complete Problem 2: Even Fibonaccis

--- lib/euler02.rb
+++ lib/euler02.rb
@@ -1,3 +1,16 @@
 def find_next_fibonacci(sequence)
   sequence[-1] + sequence[-2]
 end
+
+def find_fibonaccis(max)
+  sequence = [1,2]
+  while sequence[-1] < max
+    sequence << find_next_fibonacci(sequence)
+  end
+  sequence.pop unless sequence[-1] == max
+  return sequence
+end
+
+def sum_even_numbers(sequence)
+  sequence.select(&:even?).reduce(:+)
+end
--- test/euler_02_test.rb
+++ test/euler_02_test.rb
@@ -7,4 +7,14 @@ class EvenFibonacci < Minitest::Test
     assert_equal 34, find_next_fibonacci([1, 2, 3, 5, 8, 13, 21])
     assert_equal 89, find_next_fibonacci([1, 2, 3, 5, 8, 13, 21, 34, 55])
   end
+
+  def test_finds_fibonaccis_to_max_number
+    assert_equal [1, 2, 3, 5, 8], find_fibonaccis(10)
+    assert_equal [1, 2, 3, 5, 8, 13, 21], find_fibonaccis(25)
+    assert_equal [1, 2, 3, 5, 8, 13, 21, 34], find_fibonaccis(34)
+  end
+
+  def test_sums_even_fibonaccis_to_max_number
+    assert_equal 4613732, sum_even_numbers(find_fibonaccis(4000000))
+  end
 end
