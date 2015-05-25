92d194700f39054e89409f0c5c1e1232cc32099d
Julie Torres <reeddetorres@gmail.com>
2015-05-25 13:53:27 -0400

    Update letter counts for hundreds

--- lib/euler17.rb
+++ lib/euler17.rb
@@ -6,35 +6,39 @@ HUNDREDS_HASH = { 0 => 0, 1 => 10, 2 => 10, 3 => 12, 4 => 11, 5 => 12, 6 => 10, 
 
 def letter_count(num)
   num_chars = num.to_s.chars.to_a
-  require 'pry'; binding.pry
   if NUM_HASH.keys.include?(num)
     result = NUM_HASH[num]
   elsif num_chars.length == 3
-    hundreds = HUNDREDS_HASH[num_chars[0].to_i] || 0
-    tens = NUM_HASH[num_chars[1..2].join.to_i] || TENS_HASH[num_chars[-2].to_i]
-
-    #TODO: Fix this so that it does not count the last digit if the number is found in the NUM_HASH
+    hundreds = HUNDREDS_HASH[num_chars.shift.to_i] || 0
+    tens, num_chars = find_tens_value(num_chars)
     ones = NUM_HASH[num_chars[-1].to_i] || 0
     result = hundreds + tens + ones
-    result += 3 if uneven_hundred?(num_chars)
+    result += 3 if uneven_hundred?(num)
   else
     hundreds = 0
-    tens = NUM_HASH[num_chars[0..2].join.to_i] || TENS_HASH[num_chars[-2].to_i]
+    tens, num_chars = find_tens_value(num_chars)
     ones = NUM_HASH[num_chars[-1].to_i] || 0
     result = hundreds + tens + ones
   end
-    # test to see if it is a 3-digit number not ending in 00, if so, add 3 digits for "and"
     return result
 end
 
 def uneven_hundred?(num_chars)
-  three_digits?(num_chars) && !ends_in_zeros?(num_chars)
+  !ends_in_zeros?(num_chars)
 end
 
-def three_digits?(num_chars)
-  num_chars.length == 3
-end
-
-def ends_in_zeros?(num_chars)
+def ends_in_zeros?(num)
+  num_chars = num.to_s.chars.to_a
   num_chars[1] == "0" && num_chars[2] == "0"
 end
+
+def find_tens_value(num_chars)
+  tens = NUM_HASH[num_chars[0..1].join.to_i]
+  if tens.nil?
+    tens = TENS_HASH[num_chars[-2].to_i]
+  else
+    num_chars = 0
+  end
+  return tens, num_chars
+end
+    
