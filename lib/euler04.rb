b5c39798055155a6af13225a80373d0f740393fa
Julie Torres <reeddetorres@gmail.com>
2015-06-02 08:29:32 -0500

    Recursive solution for Euler#04: Largest Palindrome Product

--- lib/euler04.rb
+++ lib/euler04.rb
@@ -1,4 +1,4 @@
-#Set the 1st factor to 99, second to 98
+#Set the 1st factor to 99, second to 99
 #Multiply them together to get the result
 #If the result is a palindrome, return the result
 #Otherwise reduce the first factor by 1 and multiply again until you get a palindrome
@@ -7,23 +7,22 @@
 
 def largest_factors(digits)
   nines = "9" * (digits - 1)
-  [(nines + "9").to_i, (nines + "8").to_i ]
+  (nines + "9").to_i
 end
 
-def largest_palindrome_product(factors, first_factor=nil, second_factor=nil)
-  original_second_factor = factors[1]
-  first_factor ||= factors[0]
-  second_factor ||= original_second_factor
-  result = first_factor * second_factor
-  if result.to_s == result.to_s.reverse
-    return result
-  elsif first_factor == 0
-    first_factor -= 1
-    second_factor = original_second_factor
-    largest_palindrome_product(factors, first_factor, second_factor)
-  else
-    #stopping point
+def palindrome_products(highest_factor, factors = nil, palindromes=[])
+  highest_factor = highest_factor.to_i
+  return palindromes if highest_factor == 0
+  factors = (2..highest_factor).to_a
+  factors.map do |factor|
+    result = factor * highest_factor
+    palindromes << result if result.to_s == result.to_s.reverse
   end
-    
-    
+  highest_factor -= 1
+  palindrome_products(highest_factor, factors, palindromes)
 end
+
+def largest_palindrome_product(palindromes)
+  palindromes.max
+end
+
